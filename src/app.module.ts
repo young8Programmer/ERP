e8d725806f85cc68fd1fe20e397c911bfa1e4f5d
githubusername <azizbekdavronov1110@gmail.com>
2024-12-06 19:30:22 +0500

    Users and Courses

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/entities/user.entity';
+import { Course } from './courses/entities/course.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'azizbek002',
+      database: 'erp',
+      entities: [User, Course],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
