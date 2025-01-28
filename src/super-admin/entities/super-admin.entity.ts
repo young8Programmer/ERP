b7f0c927977668f01a870fbeac928464808a0f7d
Vali <valireyimbergenov79@gmail.com>
2025-01-28 17:18:30 +0500

    create

--- src/super-admin/entities/super-admin.entity.ts
+++ src/super-admin/entities/super-admin.entity.ts
@@ -1,7 +1,7 @@
 import { Profile } from 'src/profile/entities/profile.entity';
 import { Entity, PrimaryGeneratedColumn, Column, OneToOne, JoinColumn } from 'typeorm';
 
-@Entity("superAdmin")
+@Entity("superAdmins")
 export class superAdmin {
   @PrimaryGeneratedColumn()
   id: number;
