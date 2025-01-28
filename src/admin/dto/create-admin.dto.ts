821bfc0b74d90b53db52b237b57453d86c6b31bf
Vali <valireyimbergenov79@gmail.com>
2025-01-28 18:25:36 +0500

    create

--- src/admin/dto/create-admin.dto.ts
+++ src/admin/dto/create-admin.dto.ts
@@ -9,24 +9,21 @@ export class CreateAdminDto {
   @IsString()
   password: string;
 
-  @IsEmail()
-  email: string;
+  @IsString()
+  @IsNotEmpty()
+  @Length(3, 50)
+  firstName: string;
 
-   @IsString()
-    @IsNotEmpty()
-    @Length(3, 50)
-    firstName: string;
-  
-    @IsString()
-    @IsNotEmpty()
-    @Length(3, 50)
-    lastName: string;
-  
-    @IsPhoneNumber()
-    @IsNotEmpty()
-    phone: string;
-  
-    @IsString()
-    @IsNotEmpty()
-    address: string;
+  @IsString()
+  @IsNotEmpty()
+  @Length(3, 50)
+  lastName: string;
+
+  @IsPhoneNumber()
+  @IsNotEmpty()
+  phone: string;
+
+  @IsString()
+  @IsNotEmpty()
+  address: string;
 }
--- src/admin/dto/update-admin.dto.ts
+++ src/admin/dto/update-admin.dto.ts
@@ -7,25 +7,21 @@ export class UpdateAdminDto {
 
   @IsOptional()
   @IsString()
-  password?: string;
+password?: string;
+
+@IsOptional()
+  @Length(3, 50)
+  firstName?: string;
+
+  @IsString()
+  @Length(3, 50)
+  lastName?: string;
 
   @IsOptional()
-  @IsEmail()
-  email?: string;
+  @IsPhoneNumber()
+  phone?: string;
 
   @IsOptional()
-    @Length(3, 50)
-    firstName?: string;
-  
-    @IsString()
-    @Length(3, 50)
-    lastName?: string;
-  
-    @IsOptional()
-    @IsPhoneNumber()
-    phone?: string;
-  
-    @IsOptional()
-    @IsString()
-    address?: string;
+  @IsString()
+  address?: string;
 }
